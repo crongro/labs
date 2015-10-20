39b85d95a0f63eb5d1a82948365638e8b95d41ca
윤지수(Jisu Youn)/CTO/SKP <jisu.youn@sk.com>
2015-10-20 15:29:45 +0900

    Mod. code for mobile web & adjust style.

--- templateLoading/js/default.js
+++ templateLoading/js/default.js
@@ -8,11 +8,16 @@
 		var elUL = doc.querySelector("ul");
 		var nSize = 680;
 
+		var sTF = (typeof document.body.style.webkitTransform !== 'undefined') ? "webkitTransform" : 'transform';
+
 		doc.querySelector("#controller").addEventListener("click", function(ev) {
-			var preCss = elUL.style.transform;
+
+			var preCss = elUL.style[sTF];
 			var nPreX = +preCss.replace(/translate3d\((-*\d+)(px)*\,.+\)/g , "$1");
 			nNewX = nPreX + (-nSize);
-			elUL.style.transform = "translate3d("+ nNewX +"px, 0, 0)";
+
+			elUL.style[sTF] = "translate3d("+ nNewX +"px, 0, 0)";
+
 		},false);
 	}
 })(window, document);
--- templateLoading/js/lazy.js
+++ templateLoading/js/lazy.js
@@ -1,26 +1,38 @@
+(function(win, doc){
+	win.addEventListener("load", function() {
+		initAnimation();
 
-window.addEventListener("load", function() {
-	initAnimation();
-	getMoreImages();
-});
+		var elTemplate = document.querySelector('template');
+		if(elTemplate.content) getMoreImages(elTemplate);
+	});
 
-function getMoreImages() {
-	var elTemplate = document.querySelector('template');
-	var elTemplateContent = elTemplate.content;
-	var elParent = elTemplate.parentNode;
-	elParent.removeChild(elTemplate);
-	elParent.appendChild(elTemplateContent);
-}
+	function getMoreImages(elTemplate) {
+
+		console.log("start more images");
+		var elTemplateContent = elTemplate.content;
+		var elParent = elTemplate.parentNode;
+		elParent.removeChild(elTemplate);
+		elParent.appendChild(elTemplateContent);
+
+	}
 
 
-function initAnimation() {
-	var elUL = document.querySelector("ul");
-	var nSize = 680;
+	function initAnimation() {
 
-	document.querySelector("#controller").addEventListener("click", function(ev) {
-		var preCss = elUL.style.transform;
-		var nPreX = +preCss.replace(/translate3d\((-*\d+)(px)*\,.+\)/g , "$1");
-		nNewX = nPreX + (-nSize);
-		elUL.style.transform = "translate3d("+ nNewX +"px, 0, 0)";
-	},false);
-}
+		var elUL = doc.querySelector("ul");
+		var nSize = 680;
+
+		var sTF = (typeof document.body.style.webkitTransform !== 'undefined') ? "webkitTransform" : 'transform';
+
+		doc.querySelector("#controller").addEventListener("click", function(ev) {
+
+			var preCss = elUL.style[sTF];
+			var nPreX = +preCss.replace(/translate3d\((-*\d+)(px)*\,.+\)/g , "$1");
+			nNewX = nPreX + (-nSize);
+
+			elUL.style[sTF] = "translate3d("+ nNewX +"px, 0, 0)";
+
+		},false);
+
+	}
+})(window, document);
