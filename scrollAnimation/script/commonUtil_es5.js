a1abf0246a7a2a82385863d358571c931d241a3d
윤지수(Jisu Youn)/CTO/SKP <jisu.youn@sk.com>
2016-04-12 14:21:52 +0900

    added. scroll animation like Airbnb

---
+++ scrollAnimation/script/commonUtil_es5.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var _cu = {
+  getFnName: function getFnName(fn) {
+    if (typeof fn !== "function") return;
+    var sName = fn.name ? fn.name : fn.toString().match(/function\s+([^(\(|\s)]+)/)[1];
+    return sName;
+  },
+  setTranslate3dX: function setTranslate3dX(ele, nValue) {
+    var sTF = this.getCSSName('transform');
+    ele.style[sTF] = 'translate3d(' + nValue + 'px, 0, 0)';
+  },
+  getWidth: function getWidth(ele) {
+    var nWidth = 0;
+
+    if (ele.getBoundingClientRect().width) {
+      nWidth = ele.getBoundingClientRect().width;
+    } else {
+      nWidth = ele.offsetWidth;
+    }
+    return nWidth;
+  },
+
+  getCSSName: function getCSSName(sName) {
+    if (typeof this.htCacheData === "undefined") this.htCacheData = {};
+
+    if (this.htCacheData[sName]) return this.htCacheData[sName];
+
+    var _htNameSet = {
+      'transition': ['webkitTransition', 'transition'],
+      'transform': ['webkitTransform', 'transform']
+    };
+
+    var aNameList = _htNameSet[sName];
+
+    if (!this.isExist(aNameList)) return null;
+
+    for (var i = 0, len = aNameList.length; i < len; i++) {
+      if (typeof document.body.style[aNameList[i]] === 'string') this.htCacheData[sName] = aNameList[i];
+      return this.htCacheData[sName];
+    }
+  },
+
+  getTranslate3dX: function getTranslate3dX(ele) {
+    var sTF = this.getCSSName("transform");
+    var sPreCss = ele.style[sTF];
+    var nPreX = +sPreCss.replace(/translate3d\((-*\d+(?:\.\d+)*)(px)*\,.+\)/g, "$1");
+    return nPreX;
+  },
+
+  getCSSTransitionEnd: function getCSSTransitionEnd() {
+    var sTS = this.getCSSName('transition');
+    var sTSE = sTS === "webkitTransition" ? "webkitTransitionEnd" : "transitionend";
+    return sTSE;
+  },
+
+  setCSS: function setCSS(el, style, value) {
+    el.style[style] = value;
+  },
+
+  showLayer: function showLayer(el) {
+    el.style.display = "block";
+  },
+
+  closeLayer: function closeLayer(el) {
+    el.style.display = "none";
+  },
+
+  //check null or undefined
+  isExist: function isExist(data) {
+    return data != null;
+  },
+
+  isArray: function isArray(_a) {
+    if (!Array.isArray) {
+      return Object.prototype.toString.call(_a) === '[object Array]';
+    }
+    return Array.isArray(_a);
+  },
+
+  isFunction: function isFunction(fn) {
+    return Object.prototype.toString.call(fn) === '[object Function]';
+  }
+};
---
+++ scrollAnimation/script/scroll.js
@@ -0,0 +1,169 @@
+/* 
+좌에서 우측으로 background 와 동시에 width 를 변경하는 로직이 자연스럽지 않다. 
+번쩍이는 효과가 있다. 그렇다고 width만 transition 을 하니 이도저도 아니다. 레이어가 나눠서 노출되는 느낌이랄까.
+그래서 width를 늘리지 않고 width 를 100%로 채운상태에서 오히려 background 만 변경하는 것이 어떨까 한다. 물론 이것도 껌뻑거리는 느낌이 들긴하다.
+ 한가지 더 해볼 수 있는 건 width를 늘리지 않고 translateX값을 조정해서 길다란 창이 좌에서 우로 이동해서 마치 늘어리즈는 것 같은 느낌을 줄 수 있을 거 같다. 
+*/
+
+(function() {
+
+  (function() {
+    var _headerHeight = $("header").height();
+    var _searchIcon = $(".searchIcon");
+    var _inputPreviewOfIconSide = $('.inputPreviewOfIconSide');
+    var _searchView = $("#searchView");
+
+    //set icon style. 
+    iconTopPos = _headerHeight - (_searchIcon.height() / 2);
+    _searchIcon.css({'top': iconTopPos});
+    _inputPreviewOfIconSide.css({'top': iconTopPos});
+    _searchView.css({'top': iconTopPos + 31});
+  })();
+
+  var mainView = $("#mainView");
+  var headerHeight = $("header").height();
+  var inputPreviewOfIconSide = $('.inputPreviewOfIconSide');
+  var inputPreviewOfIconSideTop = parseInt(inputPreviewOfIconSide.css("top"));
+  var searchBar = $(".searchBar");
+  var searchBarInput = $('.searchBarInput');
+  var searchIcon = $(".searchIcon");
+  var searchView = $("#searchView");
+  var closeBtn = $(".closebtn");
+  var bigScroll = false;
+  var searchBarPrevColor = 'rgb(241, 159, 198)';
+  var searchBarAfterColor = '#fff';
+  var sTSE = _cu.getCSSTransitionEnd(); 
+  var inputPreview = $('.inputPreview');
+  var searchViewPreTopPos = searchView.css('top');
+
+  function onScroll() {
+    if ($(window).scrollTop() > headerHeight)  { 
+
+      if(bigScroll) return;
+
+      inputPreview.css({'opacity':1});
+
+      setTimeout(function() {
+        inputPreview.css('backgroundColor', searchBarAfterColor);
+        bigScroll = true;
+      }, 1);
+
+    } else {
+      if(!bigScroll) return;
+
+      inputPreview.css('backgroundColor', searchBarPrevColor);
+      inputPreview.css({'opacity':0});
+      bigScroll = false;
+    } 
+  }
+
+  function showSearchView(evt) {
+    searchBar.show();
+    searchBar.css({'backgroundColor': '#fff'});
+    searchBarInput.show();
+    searchIcon.hide();
+    searchView.show();
+
+    setTimeout(function() {
+      var translateDistance = $(window).scrollTop() - inputPreviewOfIconSideTop; 
+
+      searchView.css({
+        'top': '62px'
+      });
+
+    },1);
+
+    mainView.hide();
+  }
+
+  function onSearchIconTouchEnd(evt) {
+    inputPreviewOfIconSide.show();
+
+    var translateDistance = $(window).scrollTop() - inputPreviewOfIconSideTop; 
+
+    setTimeout(function(){
+      inputPreviewOfIconSide.css({
+        'transform': 'translate3d(0, ' + (translateDistance) + 'px, 0)',
+        'width': '100%',
+        'backgroundColor': '#fff',
+        'marginLeft' :0,
+        'borderRadius':0
+      });
+    },1);
+
+    mainView.css({'opacity':0});
+    searchIcon.hide();
+      
+    searchView.show();
+     setTimeout(function() {
+       searchView.css({
+         'transform': 'translate3d(0, ' + (translateDistance) + 'px, 0)'
+       });
+    },1);
+  }
+
+  function searchViewInputTransitionEnd(evt) {
+    var _name = _cu.getCSSName("transform");
+    var y = parseInt(evt.target.style[_name].split(',')[1]);
+
+    if ( y !== 0 ) {
+      //show transition 
+      searchBar.show();
+      searchBar.css({'backgroundColor': '#fff'});
+      searchBarInput.show();
+    } else {
+      //close transition 
+      searchIcon.show();
+      inputPreviewOfIconSide.css({'display': 'none'});
+    }
+  }
+
+  function onBack(evt) {
+    searchView.css(
+      {
+        'transform': 'translate3d(0,0,0)',
+        //'top': '454px'
+        'top': searchViewPreTopPos
+      }
+    );
+    searchView.hide();
+
+    searchBarInput.hide();
+    searchBar.css({'backgroundColor': 'rgb(241, 221, 231)'});
+    searchBar.hide();
+
+    if(mainView.css("display") === "none") {
+      mainView.show();
+      searchIcon.show();
+    }
+
+    mainView.css({'opacity':1});
+
+    inputPreviewOfIconSide.css({
+      'transition': 'all 800ms cubic-bezier(0.000, 1.075, 0.035, 0.915)',
+      'transform': 'translate3d(0, 0, 0)',
+      'width': '62px',
+      'backgroundColor': '#E3418E',
+      'marginLeft' : '5%',
+      'borderRadius': '50px' 
+    });
+  }
+
+  function registerEvents() {
+    $(window).scroll(onScroll);
+
+    //inputPreview
+    inputPreview.on("touchend", showSearchView);
+
+    //search Icon.
+    searchIcon.on("touchend", onSearchIconTouchEnd);
+    inputPreviewOfIconSide.on(sTSE, searchViewInputTransitionEnd);
+
+    closeBtn.on("touchend", onBack);
+  }
+
+  registerEvents();
+
+})();
+
+
